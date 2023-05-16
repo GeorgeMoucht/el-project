7ec2a82932cfe0e27cd3db58dfe68406e7f41b0b
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-05-16 15:11:04 +0300

    trying configure bootstrap and auth functionality

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
