bf362ce78e94b866d0f4cad22b5d36b8fba118eb
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-05-16 15:43:19 +0300

    Bootstrap configured properly.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,35 @@
 const mix = require('laravel-mix');
+const { VitePlugin } = require('laravel-mix-vite');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.extend('vite', new VitePlugin()).vite();
+
+mix
+    .vite({
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+            },
+        },
+        optimizeDeps: {
+            exclude: ['@livewire/livewire'],
+        },
+        build: {
+            manifest: true,
+        },
+    })
+    .js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    .webpackConfig((webpack) => {
+        return {
+            plugins: [
+                new webpack.ProvidePlugin({
+                    $: 'jquery',
+                    jQuery: 'jquery',
+                    'window.jQuery': 'jquery',
+                    Popper: ['popper.js', 'default'],
+                }),
+            ],
+        };
+    });
