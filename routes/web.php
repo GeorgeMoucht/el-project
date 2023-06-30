a783cf1809afa75316d663ba7de8d3dfe3506ed0
GeorgeMoucht <georgemoucht@gmail.com>
2023-06-30 22:33:33 +0300

    implement admin middleware to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BioElpidaController;
 use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\AdminDashboard;
+use App\Http\Middleware\AdminMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +31,11 @@ Route::get('/elpida-bio', [BioElpidaController::class, 'render'])->name('elpida-
 // Admin login routes
 Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
+
+
+Route::middleware(['admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminDashboard::class, 'dashboard'])->name('admin.dashboard');
+});
 
 // Route::middleware(['auth'])->group(function () {
 //     Route::get('/home', [HomeController::class, 'index'])->name('home');
