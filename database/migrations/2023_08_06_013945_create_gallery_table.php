8248e073979b80fbcee46a56cf63f22af42c8863
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-07 15:11:32 +0300

    make uploaded_at nullable

--- database/migrations/2023_08_06_013945_create_gallery_table.php
+++ database/migrations/2023_08_06_013945_create_gallery_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image_name')->unique();
             $table->timestamp('edited_at')->nullable();
-            $table->timestamp('uploaded_at');
+            $table->timestamp('uploaded_at')->nullable();
         });
     }
 
