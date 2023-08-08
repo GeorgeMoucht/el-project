e54e0149b1132d2c2c911f52b68c9eef4ce8bec8
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-08 04:03:15 +0300

    Storing image into files & database

--- app/Http/Controllers/AdminEditGallery.php
+++ app/Http/Controllers/AdminEditGallery.php
@@ -3,25 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\UserModel;
+use App\Models\GalleryModel;
 
 class AdminEditGallery extends Controller
 {
-
-    public function storeImage(Request $request)
-    {
-        $formData = Request::createFromGlobals()->all();
-        echo "<pre>";
-        // print_r($formData);
-        // print_r($data['user']);
-        echo session('user');
-        echo "</pre>";
-        exit;
-    }
-
-
     public function render()
     {
         return view('admin.edit-gallery');
     }
+
+    public function uploadImage(Request $request)
+    {
+        // Save the image into the storage.
+        if($request->hasFile('uploadedImage')) {
+            $destination_path = "public/images/gl";
+            $image = $request->file('uploadedImage');
+            $image_name = $image->getClientOriginalName();
+            $path = $request->file('uploadedImage')->storeAs($destination_path,$image_name);
+        
+            $input['uploadedImage'] = $image_name;
+
+            $gallery = new GalleryModel();
+            $gallery->image_name = $image_name;
+
+            $gallery->save();
+        }
+
+        // Redirect back to edit-gallery view.
+        return redirect()->route('admin.edit-gallery');
+    }
+
+
 }
