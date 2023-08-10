e018268b0ab0d88f5654220d6e8d52ec613bed08
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-11 02:44:57 +0300

    Script for gallery item modal

--- public/js/gallery-archieve-script.js
+++ public/js/gallery-archieve-script.js
@@ -1,25 +1,30 @@
-// Get the modal
-var modal = document.getElementById("modal1");
+// Execute JS after hole content of page is loaded.
+document.addEventListener('DOMContentLoaded', function() {
 
-// Get the button that opens the modal
-var btn = document.getElementById("modalBtn1 ");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  const thumbnails = document.querySelectorAll('.gallery-itm');
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  thumbnails.forEach(thumbnail => {
+      thumbnail.addEventListener('click', () => {
+          console.log('Thumbnail clicked'); // Check if this logs on click
+          const modalId = thumbnail.getAttribute('data-modal');
+          const modal = document.getElementById(modalId);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+          if (modal) {
+              modal.style.display = 'block';
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+              const closeButton = modal.querySelector('.close');
+              closeButton.addEventListener('click', () => {
+                  modal.style.display = 'none';
+              });
+
+              modal.addEventListener('click', event => {
+                if(event.target === modal) {
+                  modal.style.display = 'none';
+                }
+              });
+          }
+      });
+  });
+
+});
