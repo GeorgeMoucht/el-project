a950ffc84c2074dca195bc47075f91d0c0742a5b
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-24 22:48:16 +0300

    Init contact blade file

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+<!-- <link href="{{ asset('css/gallery.css') }}" rel="stylesheet"> -->
+@section('content')
+
+<h1>contact</h1>
+@endsection
+
+@section('pagespecificscripts')
+<!-- <script src="{{ asset('js/gallery-script.js') }}"></script> -->
+<!-- <script src="{{ asset('js/gallery-archieve-script.js') }}"></script> -->
+@endsection
