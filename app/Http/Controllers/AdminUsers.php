f74dda9a0fddd1fe4dfee9092cdb2ec0a255b697
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-29 21:30:31 +0300

    Implement deleteUser function

--- app/Http/Controllers/AdminUsers.php
+++ app/Http/Controllers/AdminUsers.php
@@ -31,4 +31,18 @@ class AdminUsers extends Controller
         // exit;
         return view('admin.users', ['users'=>$data]);
     }
+
+    // Delete user function
+    public function deleteUser($id)
+    {
+        try {
+            // Find the user record by ID and delete it if exists.
+            UserModel::findOrFail($id)->delete();
+            session()->flash('flashMessage', 'Ο χρήστης διαγράφηκε επιτυχώς.');
+        } catch (\Exception $e) {
+            session()->flash('flashMessageDanger', 'Κάτι πήγε στραβά... Παρακαλώ προσπαθήστε πάλι.');
+        }
+        
+        return redirect()->route('admin.users'); // Redirect back tou admin edit user page.
+    }
 }
