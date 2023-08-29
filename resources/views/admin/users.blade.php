aee91426faf14cfbf7f95e488f38e343b2723d20
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-29 21:30:57 +0300

    Add flash messages and delete comments

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -8,6 +8,20 @@
     <!-- https://www.bootdey.com/snippets/view/manage-users#html -->
     <div class="container">
         
+
+
+    @if(session('flashMessage'))
+        <div class="alert alert-success">
+            {{ session('flashMessage') }}
+        </div>
+    @endif
+
+    @if(session('flashMessageDanger'))
+        <div class="alert alert-danger">
+            {{ session('flashMessageDanger') }}
+        </div>
+    @endif
+
     <div class="row mt-4">
         <div class="col-md-12">
             <div class="card"  style="height: 400px;">
@@ -68,7 +82,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary close-modal-btn" data-bs-dismiss="modal{{ $user->id }}">Πίσω</button>
-                            <form method="post" action="{{ route('admin.edit-gallery.delete', ['id' => $user->id]) }}">
+                            <form method="post" action="{{ route('admin.users.delete', ['id' => $user->id]) }}">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Διαγραφή</button>
@@ -78,66 +92,6 @@
                 </div>
             </div>
         @endforeach
-
-
-        <!-- <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-uppercase mb-0">Διαχείρηση Χρηστών</h5>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table no-wrap user-table mb-0">
-                            <thead>
-                                <tr>
-                                    <th scope="col" class="border-0 text-uppercase font-medium pl-4">#</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Όνομα</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Email</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Κωδικός</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Είδος</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Εγγραφή</th>
-                                    <th scope="col" class="border-0 text-uppercase font-medium">Διαχείρηση</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($users as $user)
-                                    <tr>
-                                        <td class="pl-4">{{ $user->id }}</td>
-                                        <td>
-                                            <h5 class="font-medium mb-0">{{ $user->name }}</h5>
-                                        </td>
-                                        <td>
-                                            <span class="text-muted">{{ $user->email }}</span>
-                                        </td>
-                                        <td>
-                                            <span class="text-muted">Reset Password</span>
-                                        </td>
-                                        <td>
-                                            @if($user->admin === 1)
-                                                <span class="text-muted">Διαχειριστής</span>
-                                            @elseif ($user->admin === 0)
-                                                <span class="text-muted">Χρήστης</span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($user->created_at === null)
-                                                <span class="text-muted">-</span>
-                                            @else
-                                                <span class="text-muted">{{ $user->created_at }}</span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-outline-info btn-circle btn-lg btn-circle ml-2"><i class="fa fa-edit"></i> </button>
-                                            <button type="button" class="btn btn-outline-info btn-circle btn-lg btn-circle ml-2" style="margin-left: .5rem;"><i class="fa fa-trash"></i> </button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 
 @endsection
