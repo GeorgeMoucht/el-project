86d5dbfa1d9aa55b0a07a8a8921760483d59a76d
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-08-29 21:30:20 +0300

    Create user delete route

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminDashboard::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/admin/users', [AdminUsers::class, 'render'])->name('admin.users');
+    Route::delete('/admin/users/delete/{id}', [AdminUsers::class, 'deleteUser'])->name('admin.users.delete');
     Route::get('/admin/edit-gallery', [AdminEditGallery::class, 'render'])->name('admin.edit-gallery');
     Route::post('/admin/edit-gallery/upload', [AdminEditGallery::class, 'uploadImage'])->name('admin.edit-gallery.upload');
     Route::delete('/admin/edit-gallery/delete/{id}', [AdminEditGallery::class, 'delete'])->name('admin.edit-gallery.delete');
