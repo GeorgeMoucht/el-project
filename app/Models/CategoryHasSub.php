10b55d9922cbf56bb5ea240b21a457fc8f6b196c
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-09-23 17:24:11 +0300

    Init CategoryHasSub class

---
+++ app/Models/CategoryHasSub.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryHasSub extends Model
+{
+    use HasFactory;
+
+    protected $table = 'category_has_sub'; // Specify the correct table name in Database.
+
+    protected $fillable = [
+        'categoryId',
+        'subCategoryId',
+    ];
+
+    // Define the relationship with the 'categories' table
+    public function category()
+    {
+        return $this->belongsTo(CategoryModel::class, 'categoryId');
+    }
+
+    // Define the relationship with the 'subCategories' table
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategoryModel::class, 'subCategoryId');
+    }
+}
